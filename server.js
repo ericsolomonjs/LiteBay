8494cd9e0258caf8bbd8f1ef437c44416eb041d8
Sid Natarajan <sid.nat@gmail.com>
2022-11-22 21:46:55 +0000

    added bcrypt/cookies and login/register routes

--- server.js
+++ server.js
@@ -5,6 +5,16 @@ require('dotenv').config();
 const sassMiddleware = require('./lib/sass-middleware');
 const express = require('express');
 const morgan = require('morgan');
+const cookieSession = require("cookie-session");
+
+// UPDATE WITH CORRECT FUNCTIONS import helper fx from single file
+const {
+  generateRandomString,
+  getUserByEmail,
+  getUserByUserId,
+  urlsForUser
+} = require("./helpers");
+const bcrypt = require("bcryptjs");
 
 const PORT = process.env.PORT || 8080;
 const app = express();
@@ -25,6 +35,11 @@ app.use(
   })
 );
 app.use(express.static('public'));
+app.use(cookieSession({
+  name: "session",
+  keys: ["fdj3i42o2k3ggdger644212"],
+  maxAge: 24 * 60 * 60 * 1000
+}));
 
 // Separated Routes for each Resource
 // Note: Feel free to replace the example routes below with your own
@@ -47,6 +62,85 @@ app.use('/users', usersRoutes);
 app.get('/', (req, res) => {
   res.render('index');
 });
+
+
+
+
+
+// renders login page
+app.get("/login", (req, res) => {
+  const loggedIn = getUserByUserId(req.session.user_id, users);
+
+  // if logged in, redirect to "my urls"
+  if (loggedIn) {
+    return res.redirect("/urls");
+  }
+  return res.render("urls_login");
+});
+
+// user enters information and logs in
+app.post("/login", (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+  const authorizedUser = getUserByEmail(users, email);
+
+  if (!authorizedUser) {
+    return res.status(403).send("<p>User with that e-mail cannot be found.</p>");
+  }
+
+  // if password does not match with stored value, return error message
+  if (!bcrypt.compareSync(password, authorizedUser.password)) {
+    return res.status(403).send("<p>Password does not match.</p>");
+  } else {
+    req.session.user_id = authorizedUser.id;
+    return res.redirect("/urls");
+  }
+});
+
+// logs user out, clears session cookies, redirect to login page
+app.post("/logout", (req, res) => {
+  req.session = undefined;
+  return res.redirect("/login");
+});
+
+// renders user registration page
+app.get("/register", (req, res) => {
+  const userID = req.session.user_id;
+  const loggedIn = getUserByUserId(userID, users);
+
+  if (loggedIn) {
+    return res.redirect("/urls");
+  }
+
+  return res.render("urls_register");
+});
+
+// user submits data for registration, account is created
+app.post("/register", (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  // if either text field is empty, return error message
+  if (!email || !password) {
+    return res.status(400).send("<p>Email or Password field was left blank.</p>");
+  }
+
+  // if email exists in database, return error message
+  if (getUserByEmail(users, email)) {
+    return res.status(400).send("<p>Email is already in use.</p>");
+  }
+
+  const newUserId = generateRandomString();
+  users[newUserId] = {
+    id:  newUserId,
+    email,
+    password: bcrypt.hashSync(password, 10)
+  };
+  req.session.user_id = newUserId;
+
+  return res.redirect("/urls");
+});
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
