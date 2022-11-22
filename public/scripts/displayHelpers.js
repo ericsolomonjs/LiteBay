d4be614397aa32b495b84a57ba05376cec737bf8
facelessagony <eric.solomon.ca@gmail.com>
2022-11-22 13:06:47 +0000

    comment out messages fns for now

--- public/scripts/displayHelpers.js
+++ public/scripts/displayHelpers.js
@@ -15,8 +15,8 @@ function getUserInfo(username) {
   let {email,full_name, id, is_Admin} = '';
   pool.query(`SELECT username,email,full_name, id, is_admin FROM users WHERE username='${username}'`, (err, res) => {
     if (err) {console.log(err);}
-    id = res.id
-    is_Admin = res.is_Admin
+    id = res.id;
+    is_Admin = res.is_Admin;
     email = res.email;
     full_name = res.full_name;
   })
@@ -27,60 +27,70 @@ function getUserListings(username) {
   //get the listings for a user from username (JOIN TABLES)
   let listings = {};
   let listingsArray = [];
+  const value = [username]
+  const query = [`SELECT * FROM listings JOIN users ON listings.user_id = users.id WHERE username='$1'`]
   //get matching listings by username
-  pool.query(`SELECT id FROM listings JOIN users ON listings.user_id = users.id WHERE username='${username}'`, (req, res) => {
-    listingsArray = res.id; //check if possible to use array for id's
-  })
-  //get values for each listing
-  for (let listing in listingsArray) {
-    pool.query(`SELECT * FROM listings WHERE id='${listing}'`, (req, res) => {
-      listings[res.id] = {
-        image: res.image,
-        text: res.text,
-        price: res.price,
-        featured: res.featured,
-        sold: res.sold,
-        date_added: res.date_added
-      }
-    })
-  }
-  
+  pool.query(query, value, (err, res) => {
+    if (err) {
+    console.log(err);
+    return;
+    }
+
+    listingsArray = res; //check if possible to use array for id's
+  })  
   return listings;
+}
+
+function getUserInfo (username){
+  const query = `SELECT * FROM users WHERE username = $1`;
+  const values = [username];
+  let data;
+
+  pool.query(query, values)
+  .then ((res, error) => {
+    if (error) {console.log("ERROR: ", error);}
+    data = res;
+    console.log(res);
+
+
+  })
+
 }
 
 function displayUserInfo(infoObject) {
 
 }
 
-function getMessages(username) {
-  //get the messages for a user from username (join tables)
-  let {receivedMessages, sentMessages} = null;
-  pool.query(`SELECT messages.id FROM messages JOIN users ON messages.sent_to = users.id WHERE username = ${username}`, (req, res) => {
-    receivedMessages = res.id;
-  });
-  pool.query(`SELECT messages.id FROM messages JOIN users ON messages.sent_from = users.id WHERE username = ${username}`, (req, res) => {
-    sentMessages = res.id;
-  });
-  //return messages object  
-  return {receivedMessages, sentMessages};
-
-}
-
-function displayMessages(messagesObject) {
-  
-}
-
 function getFavorites(username) {
   let favorites = null;
-  let query
-  let values
-  pool.query(`SELECT listings_id FROM favorites JOIN users ON favorites.user_id=users.id WHERE username='${username}'`,(req, res) => {
+  const query =`SELECT listings_id FROM favorites JOIN users ON favorites.user_id=users.id WHERE username='$1'`
+  let values = [username];
+  pool.query(query,values)
+  then((req, res) => {
     favorites = res.listings.id;
   })
   return favorites;
 }
 
-
-function displayFavorites(favoritesObject){
+function displayListings(favoritesObject){
   
 }
+
+//commented out messages for now
+// function getMessages(username) {
+//   //get the messages for a user from username (join tables)
+//   let {receivedMessages, sentMessages} = null;
+//   pool.query(`SELECT messages.id FROM messages JOIN users ON messages.sent_to = users.id WHERE username = ${username}`, (req, res) => {
+//     receivedMessages = res.id;
+//   });
+//   pool.query(`SELECT messages.id FROM messages JOIN users ON messages.sent_from = users.id WHERE username = ${username}`, (req, res) => {
+//     sentMessages = res.id;
+//   });
+//   //return messages object  
+//   return {receivedMessages, sentMessages};
+
+// }
+
+// function displayMessages(messagesObject) {
+  
+// }
