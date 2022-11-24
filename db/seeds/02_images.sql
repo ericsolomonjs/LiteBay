3af83e472eaa97f5ab62cc48cca68a48d1eedb8e
facelessagony <eric.solomon.ca@gmail.com>
2022-11-24 21:57:39 +0000

    clean up seeds

--- db/seeds/02_images.sql
+++ db/seeds/02_images.sql
@@ -1,3 +1,7 @@
-INSERT INTO images (url, alt_text) VALUES ('http://url.com/1.jpg', 'Image of a motorcycle');
-INSERT INTO images (url, alt_text) VALUES ('http://url.com/2.jpg', 'Image of a car');
-INSERT INTO images (url, alt_text) VALUES ('http://url.com/3.jpg', 'Image of a lawnmower');
+INSERT INTO images (url, alt_text) VALUES ('https://lh5.googleusercontent.com/p/AF1QipM0WcDcc0m9DXoPO4c5eBcUreIsZ8dyH3Lgzjr8=w92-h92-n-k-no', 'Image of a car');
+INSERT INTO images (url, alt_text) VALUES ('https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcTaf3My6-1qSKzdFcTLyZr1ORFhYqAET3lsVOIGek5IeFuQ4TYPs7qaJApKWywa-hSGaL1SiqQ1Q9XLPy7X24geU0TpUXmRalKmOBJgwV_qjmTvpf0Xl1e0H5AjUM3J0kiTKY-sh2fE&usqp=CAc', 'Image of a motorcycle');
+INSERT INTO images (url, alt_text) VALUES ('https://hips.hearstapps.com/hmg-prod/images/mower-1568302588.jpg', 'Image of a lawnmower');
+INSERT INTO images (url, alt_text) VALUES ('https://scootercountry.ca/wp-content/uploads/2019/04/pride-mobility-raptor-fast-scooter-main-green-700x700.jpg', 'Image of a scooter');
+INSERT INTO images (url, alt_text) VALUES ('https://hips.hearstapps.com/delish/assets/18/08/1519321899-hard-boiled-eggs-horizontal.jpg', 'Image of hard boiled ehhs');
+INSERT INTO images (url, alt_text) VALUES ('https://recipes.timesofindia.com/thumb/60018142.cms', 'Image of an egg sandwich');
+INSERT INTO images (url, alt_text) VALUES ('https://therecipecritic.com/wp-content/uploads/2019/02/besteggsalad.jpg', 'Image of an egg salad');
--- db/seeds/03_listings.sql
+++ db/seeds/03_listings.sql
@@ -1,7 +1,7 @@
-INSERT INTO listings (image_id, listing_title, text, price, user_id, featured, sold, date_added) VALUES (1, 'Test title 1', 'This is a listing for a product that is being sold on this website.', 99.99, 3, TRUE, FALSE, '2022-06-06');
-INSERT INTO listings (image_id, listing_title, text, price, user_id, featured, sold, date_added) VALUES (2, 'Test title 2', 'This is a listing for a product that is being sold on this website.', 50, 2, FALSE, TRUE, '2022-06-06');
-INSERT INTO listings (image_id, listing_title, text, price, user_id, featured, sold, date_added) VALUES (3, 'Test title 3', 'This is a listing for a product that is being sold on this website.', 20, 1, TRUE, TRUE, '2022-06-06');
-INSERT INTO listings (image_id, listing_title, text, price, user_id, featured, sold, date_added) VALUES (2, 'Test title 4', 'This is a listing for a product that is being sold on this website.', 20, 1, TRUE, TRUE, '2022-06-06');
-INSERT INTO listings (image_id, listing_title, text, price, user_id, featured, sold, date_added) VALUES (1, 'hard boiled egg', 'Buy eggs!', 4, 6, FALSE, FALSE, '2020-06-02');
-INSERT INTO listings (image_id, listing_title, text, price, user_id, featured, sold, date_added) VALUES (1, 'egg sandwich', 'Buy eggs!', 3, 5, FALSE, FALSE, '2004-11-08');
-INSERT INTO listings (image_id, listing_title, text, price, user_id, featured, sold, date_added) VALUES (1, 'egg sandwich', 'Buy eggs!', 8, 3, FALSE, FALSE, '2004-08-06');
+INSERT INTO listings (image_id, listing_title, text, price, user_id, featured, sold, date_added) VALUES (1, 'Car', 'Listing Text.', 99.99, 3, TRUE, FALSE, '2022-06-06');
+INSERT INTO listings (image_id, listing_title, text, price, user_id, featured, sold, date_added) VALUES (2, 'Motorcycle', 'Listing Text.', 50, 2, FALSE, TRUE, '2022-06-06');
+INSERT INTO listings (image_id, listing_title, text, price, user_id, featured, sold, date_added) VALUES (3, 'Lawnwower', 'Listing Text.', 20, 1, TRUE, TRUE, '2022-06-06');
+INSERT INTO listings (image_id, listing_title, text, price, user_id, featured, sold, date_added) VALUES (4, 'Scooter', 'Listing Text.', 20, 1, TRUE, TRUE, '2022-06-06');
+INSERT INTO listings (image_id, listing_title, text, price, user_id, featured, sold, date_added) VALUES (5, 'Hard Boiled Eggs', 'Buy eggs!', 4, 6, FALSE, FALSE, '2020-06-02');
+INSERT INTO listings (image_id, listing_title, text, price, user_id, featured, sold, date_added) VALUES (6, 'Egg Sandwich', 'Buy eggs!', 3, 5, FALSE, FALSE, '2004-11-08');
+INSERT INTO listings (image_id, listing_title, text, price, user_id, featured, sold, date_added) VALUES (7, 'Egg Salad', 'Buy eggs!', 8, 3, FALSE, FALSE, '2004-08-06');
