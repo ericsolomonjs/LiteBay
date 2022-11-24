cb0bdc970e62386b307ae135d2d81af49cd76ec0
facelessagony <eric.solomon.ca@gmail.com>
2022-11-24 02:44:04 +0000

    add user route file w cookie check

---
+++ routes/user.js
@@ -0,0 +1,19 @@
+/*
+ * All routes for User are defined here
+ * Since this file is loaded in server.js into /user,
+ *   these routes are mounted onto /user
+ * See: https://expressjs.com/en/guide/using-middleware.html#middleware.router
+ */
+
+const express = require('express');
+const router  = express.Router();
+
+router.get('/', (req, res) => {
+  if (req.session.user_id) {
+    res.render('user_page');
+  } else {
+    res.redirect('login');
+  }
+}); 
+
+module.exports = router;
