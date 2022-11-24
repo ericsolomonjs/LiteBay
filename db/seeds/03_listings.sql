c4b90019aa32972a9beeedac489e87075f7b39f1
Ryan Zhen <rjhzhen@gmail.com>
2022-11-24 00:08:24 -0500

    Fix problem with generated seeds

--- db/seeds/03_listings.sql
+++ db/seeds/03_listings.sql
@@ -3,14 +3,6 @@ INSERT INTO listings (id, image_id, listing_title, text, price, user_id, feature
 INSERT INTO listings (id, image_id, listing_title, text, price, user_id, featured, sold, date_added) VALUES (3, 3, 'Test title 3', 'This is a listing for a product that is being sold on this website.', 20, 1, TRUE, TRUE, '2022-06-06');
 INSERT INTO listings (id, image_id, listing_title, text, price, user_id, featured, sold, date_added) VALUES (4, 2, 'Test title 4', 'This is a listing for a product that is being sold on this website.', 20, 1, TRUE, TRUE, '2022-06-06');
 
-INSERT INTO listings (id, image_id, listing_title, text, price, user_id, featured, sold, date_added) VALUES (5, https://tinyurl.com/eggfancy, 'hard boiled egg', 'Buy eggs!', 4, 6, FALSE, FALSE, 2020-6-2);
-INSERT INTO listings (id, image_id, listing_title, text, price, user_id, featured, sold, date_added) VALUES (6, https://tinyurl.com/eggfancy, 'egg sandwich', 'Buy eggs!', 3, 5, FALSE, FALSE, 2004-11-8);
-INSERT INTO listings (id, image_id, listing_title, text, price, user_id, featured, sold, date_added) VALUES (7, https://tinyurl.com/eggfancy, 'egg sandwich', 'Buy eggs!', 8, 3, FALSE, FALSE, 2004-8-6);
-INSERT INTO listings (id, image_id, listing_title, text, price, user_id, featured, sold, date_added) VALUES (8, https://tinyurl.com/eggfancy, 'scrambled eggs', 'Buy eggs!', 5, 3, FALSE, FALSE, 2018-6-2);
-INSERT INTO listings (id, image_id, listing_title, text, price, user_id, featured, sold, date_added) VALUES (9, https://tinyurl.com/eggfancy, 'undefined', 'Buy eggs!', 10, 2, FALSE, FALSE, 2005-5-8);
-INSERT INTO listings (id, image_id, listing_title, text, price, user_id, featured, sold, date_added) VALUES (10, https://tinyurl.com/eggfancy, 'scrambled eggs', 'Buy eggs!', 1, 5, FALSE, FALSE, 2021-1-5);
-INSERT INTO listings (id, image_id, listing_title, text, price, user_id, featured, sold, date_added) VALUES (11, https://tinyurl.com/eggfancy, 'scrambled eggs', 'Buy eggs!', 4, 6, FALSE, FALSE, 2014-9-5);
-INSERT INTO listings (id, image_id, listing_title, text, price, user_id, featured, sold, date_added) VALUES (12, https://tinyurl.com/eggfancy, 'hard boiled egg', 'Buy eggs!', 10, 4, FALSE, FALSE, 2021-8-1);
-INSERT INTO listings (id, image_id, listing_title, text, price, user_id, featured, sold, date_added) VALUES (13, https://tinyurl.com/eggfancy, 'scrambled eggs', 'Buy eggs!', 1, 1, FALSE, FALSE, 2017-6-8);
-INSERT INTO listings (id, image_id, listing_title, text, price, user_id, featured, sold, date_added) VALUES (14, https://tinyurl.com/eggfancy, 'undefined', 'Buy eggs!', 6, 6, FALSE, FALSE, 2018-1-7);
-INSERT INTO listings (id, image_id, listing_title, text, price, user_id, featured, sold, date_added) VALUES (15, https://tinyurl.com/eggfancy, 'undefined', 'Buy eggs!', 7, 4, FALSE, FALSE, 2002-8-4);
+INSERT INTO listings (id, image_id, listing_title, text, price, user_id, featured, sold, date_added) VALUES (5, 1, 'hard boiled egg', 'Buy eggs!', 4, 6, FALSE, FALSE, '2020-06-02');
+INSERT INTO listings (id, image_id, listing_title, text, price, user_id, featured, sold, date_added) VALUES (6, 1, 'egg sandwich', 'Buy eggs!', 3, 5, FALSE, FALSE, '2004-11-08');
+INSERT INTO listings (id, image_id, listing_title, text, price, user_id, featured, sold, date_added) VALUES (7, 1, 'egg sandwich', 'Buy eggs!', 8, 3, FALSE, FALSE, '2004-08-06');
