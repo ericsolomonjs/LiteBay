a5d01e6eda976ab91389e09e5010e03a3d6fa2d8
facelessagony <eric.solomon.ca@gmail.com>
2022-11-24 20:05:27 +0000

    remove unnecessary code from app.js

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,45 +1,27 @@
-//   getUserInfo,
-//   getUserListings,
-//   getUserInfo,
-//   displayUserInfo,
-//   getFavorites
-
-const { buildAdminPage } = require("../../bin/helpers/pageBuilderHelper");
-
-
 // Client facing scripts here
+
 $(document).ready(() => {
   //on document ready load all the listings user etc
   const filename = (document.URL.split('/').pop() ? document.URL.split('/').pop() : "local") ;
   console.log('filename = ', filename );
   //populate homepage with assets
   if (filename === "local") {
-    buildHomePage();
+    console.log("before buildhomepage,inside condition");
+    loadFeaturedListings();
+    loadUnfeaturedListings();
     
-    // const featuredListings = getFeaturedListings();
-    // const listings = getListings();
-
-    // //loop through and display each featured listing
-    // for (let listing in featuredListings) {
-    //   $('div.featured-section').prepend(displayListings(listing));
-    // }
-
-    // //loop through and display each listing
-    // for (let listing in listings) {
-    //   $('div.listings-section').prepend(displayListings(listing));
-    // }
   }
 
   //populate user page with assets
   if (filename === "user") {
     buildUserPage();
     
-    // const favoriteListings = getFavoriteListings();
+    // const favouriteListings = getFavouriteListings();
     // const userInfo = getUserInfo();
     // displayUserInfo(userInfo);
 
-    // //loop through and display each favorited listing
-    // for (let listing in favoriteListings) {
+    // //loop through and display each favourited listing
+    // for (let listing in favouriteListings) {
     //   $('div.featured-section').prepend(displayListings(listing));
     // }
   }
