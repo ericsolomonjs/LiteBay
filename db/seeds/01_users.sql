0e44743a6054dce20bef168de91ed18d0e48a3ac
Sid Natarajan <sid.nat@gmail.com>
2022-11-25 10:36:04 +0000

    hashed user passwords now work

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,8 +1,8 @@
-INSERT INTO users (username, email, hashed_password, full_name, is_admin) VALUES ('Admin', 'admin@gmail.com', '$2a$10$44aZdcqrJQyIbU0xneIrg.FTsMLKtbPIwsfDiNfUFgjQAdvlJ4016', 'LHL Admin', TRUE);
-INSERT INTO users (username, email, hashed_password, full_name, is_admin) VALUES ('KikiChaos', 'kiki@chaos.com', 'hashedPass', 'Elizabeth Warren', TRUE);
-INSERT INTO users (username, email, hashed_password, full_name) VALUES ('Carl', 'carl@carl.com', 'hashedPass', 'Carl Carlington');
-INSERT INTO users (username, email, hashed_password, full_name) VALUES ('Bob', 'bob@bob.com', 'hashedPass', 'Bob Bobberton');
-INSERT INTO users (username, email, hashed_password, full_name, is_admin) VALUES ('Steve', 'steve@litebay.org', 'hashedPass', 'Steve Stevenson', TRUE);
-INSERT INTO users (username, email, hashed_password, full_name) VALUES ('Doug', 'doug@abc.com', 'hashedPass', 'Doug the Carpenter');
-INSERT INTO users (username, email, hashed_password, full_name) VALUES ('Elliott', 'e.bay@online.com', 'hashedPass', 'Elliott Bayes');
-INSERT INTO users (username, email, hashed_password, full_name) VALUES ('Hubu', 'store@helloitis.me', 'hashedPass', 'H. Haba');
+INSERT INTO users (username, email, hashed_password, full_name, is_admin) VALUES ('admin', 'admin@gmail.com', '$2a$10$44aZdcqrJQyIbU0xneIrg.FTsMLKtbPIwsfDiNfUFgjQAdvlJ4016', 'LHL Admin', TRUE);
+INSERT INTO users (username, email, hashed_password, full_name) VALUES ('KikiChaos', 'kiki@chaos.com', '$2a$10$utsgw6n80a2/hv1W42GrueW9VRpuqZkND46ivUuRQ63d6cgJkyRB2', 'Elizabeth Warren');
+INSERT INTO users (username, email, hashed_password, full_name) VALUES ('Carl', 'carl@carl.com', '$2a$10$aRTIEBJAsd6oNa8uD4VxYOcv1gYdIkz/A/u1dlrX/3m.URXZCwVO2', 'Carl Carlington');
+INSERT INTO users (username, email, hashed_password, full_name) VALUES ('Bob', 'bob@bob.com', '$2a$10$Q7LCYNphvtnjo/rWVAD58.um8u4Qj47L5ndCyAo6Q/FMUwl9NDT5m', 'Bob Bobberton');
+INSERT INTO users (username, email, hashed_password, full_name) VALUES ('Steve', 'steve@litebay.org', '$2a$10$9Xe3I2vTxfa0vcFH/ppdUe3ghDnDti7Kntml1/ed0.2iZ/PNQ7zmq', 'Steve Stevenson');
+INSERT INTO users (username, email, hashed_password, full_name) VALUES ('Doug', 'doug@abc.com', '$2a$10$zOxVc.Ia76/B7QNXbOlpweuqhP4UeznzPXIvQFFnakbickZmy1kne', 'Doug the Carpenter');
+INSERT INTO users (username, email, hashed_password, full_name) VALUES ('Elliott', 'e.bay@online.com', '$2a$10$NtVbEYl5a1ZvjHsRw4e9De0kpYMlbrmF5.jHQxv8x8I5BwRZG0egi', 'Elliott Bayes');
+INSERT INTO users (username, email, hashed_password, full_name) VALUES ('Hubu', 'store@helloitis.me', '$2a$10$lQ1dEv9aq10jtmP2wqLO3eSb5RlNOt3mAafldPxSWJtOSmif.WpLm', 'H. Haba');
