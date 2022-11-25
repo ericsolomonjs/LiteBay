388f438409a8488641c8553ee2415845c1119938
facelessagony <eric.solomon.ca@gmail.com>
2022-11-25 04:37:52 +0000

    modified listings seeds

--- db/seeds/03_listings.sql
+++ db/seeds/03_listings.sql
@@ -1,7 +1,7 @@
-INSERT INTO listings (image_id, listing_title, text, price, user_id, featured, sold, date_added) VALUES (1, 'Car', 'Listing Text.', 99.99, 1, TRUE, FALSE, '2022-06-06');
-INSERT INTO listings (image_id, listing_title, text, price, user_id, featured, sold, date_added) VALUES (2, 'Motorcycle', 'Listing Text.', 50, 1, FALSE, TRUE, '2022-06-06');
-INSERT INTO listings (image_id, listing_title, text, price, user_id, featured, sold, date_added) VALUES (3, 'Lawnwower', 'Listing Text.', 20, 1, TRUE, TRUE, '2022-06-06');
-INSERT INTO listings (image_id, listing_title, text, price, user_id, featured, sold, date_added) VALUES (4, 'Scooter', 'Listing Text.', 20, 1, TRUE, TRUE, '2022-06-06');
-INSERT INTO listings (image_id, listing_title, text, price, user_id, featured, sold, date_added) VALUES (5, 'Hard Boiled Eggs', 'Buy eggs!', 4, 1, FALSE, FALSE, '2020-06-02');
-INSERT INTO listings (image_id, listing_title, text, price, user_id, featured, sold, date_added) VALUES (6, 'Egg Sandwich', 'Buy eggs!', 3, 1, FALSE, FALSE, '2004-11-08');
-INSERT INTO listings (image_id, listing_title, text, price, user_id, featured, sold, date_added) VALUES (7, 'Egg Salad', 'Buy eggs!', 8, 1, FALSE, FALSE, '2004-08-06');
+INSERT INTO listings (image_id, listing_title, text, price, user_id, featured, sold, date_added) VALUES (1, 'Car', 'Listing Text.', 99.99, 1, TRUE, FALSE, 2022-06-06);
+INSERT INTO listings (image_id, listing_title, text, price, user_id, featured, sold, date_added) VALUES (2, 'Motorcycle', 'Listing Text.', 50, 1, FALSE, TRUE, 2022-06-06);
+INSERT INTO listings (image_id, listing_title, text, price, user_id, featured, sold, date_added) VALUES (3, 'Lawnwower', 'Listing Text.', 20, 1, TRUE, TRUE, 2022-06-06);
+INSERT INTO listings (image_id, listing_title, text, price, user_id, featured, sold, date_added) VALUES (4, 'Scooter', 'Listing Text.', 20, 1, TRUE, TRUE, 2022-06-06);
+INSERT INTO listings (image_id, listing_title, text, price, user_id, featured, sold, date_added) VALUES (5, 'Hard Boiled Eggs', 'Buy eggs!', 4, 1, FALSE, FALSE, 2020-06-02);
+INSERT INTO listings (image_id, listing_title, text, price, user_id, featured, sold, date_added) VALUES (6, 'Egg Sandwich', 'Buy eggs!', 3, 1, FALSE, FALSE, 2004-11-08);
+INSERT INTO listings (image_id, listing_title, text, price, user_id, featured, sold, date_added) VALUES (7, 'Egg Salad', 'Buy eggs!', 8, 1, FALSE, FALSE, 2004-08-06);
